3f631e7c240475c5ef933ef286fcd54aab7a7f34
Maxim Tsurkanov <max86ts@gmail.com>
2022-04-09 21:36:34 +0300

    3. Класс хранилища пользователей UserStorage [#1104 #286308].Refactoring

--- src/main/java/concurrent/storage/UserStorage.java
+++ src/main/java/concurrent/storage/UserStorage.java
@@ -3,68 +3,48 @@ package concurrent.storage;
 import net.jcip.annotations.GuardedBy;
 import net.jcip.annotations.ThreadSafe;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 @ThreadSafe
 public class UserStorage {
 
     @GuardedBy("this")
-    private final List<User> stoge;
+    private final Map<Integer, User> stoge;
 
     public UserStorage() {
-        stoge = new ArrayList<>();
+        stoge = new HashMap<>();
     }
 
     public synchronized boolean add(User user) {
-        boolean rsl = false;
-        if (!stoge.contains(user)) {
-            stoge.add(user);
-            rsl = true;
-        }
-        return rsl;
-    }
-
-    private synchronized int findIndexById(int id) {
-        int rsl = -1;
-        for (int index = 0; index < stoge.size(); index++) {
-            if (stoge.get(index).getId() == id) {
-                rsl = index;
-            }
-        }
-        return rsl;
+        return stoge.putIfAbsent(user.getId(), user) != null;
     }
 
     public synchronized boolean update(User user) {
-        boolean rsl = false;
-        int index = findIndexById(user.getId());
-        if (index != -1) {
-            stoge.set(index, user);
-            rsl = true;
-        }
-        return rsl;
+        return stoge.put(user.getId(), user) != null;
     }
 
     public synchronized boolean delete(User user) {
-        return stoge.remove(user);
+        return stoge.remove(user.getId()) != null;
     }
 
     public synchronized boolean transfer(int fromId, int toId, int amount) {
         boolean rsl = false;
-        int indexFrom = findIndexById(fromId);
-        int indexTo = findIndexById(toId);
-            if (indexFrom != -1 && indexTo != -1) {
-                User userFrom = stoge.get(indexFrom);
+            if (stoge.containsKey(fromId) && stoge.containsKey(toId)) {
+                User userFrom = stoge.get(fromId);
                 int amountFrom = userFrom.getAmount();
                 if (amountFrom >= amount) {
                     userFrom.setAmount(amountFrom - amount);
                     update(userFrom);
-                    User userTo = stoge.get(indexTo);
+                    User userTo = stoge.get(toId);
                     userTo.setAmount(userTo.getAmount() + amount);
                     update(userTo);
                     rsl = true;
                 }
             }
         return rsl;
+    }
+
+    public synchronized List<User> getUsers() {
+        return new ArrayList<>(stoge.values());
     }
 }
