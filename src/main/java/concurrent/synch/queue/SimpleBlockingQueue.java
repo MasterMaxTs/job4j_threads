b0cce86d04d38da2eeec4c5d5e61943725763049
Maxim Tsurkanov <max86ts@gmail.com>
2022-04-15 14:55:40 +0300

    1. Реализовать шаблон Producer Consumer. [#1098 #288712].Refactoring

--- src/main/java/concurrent/synch/queue/SimpleBlockingQueue.java
+++ src/main/java/concurrent/synch/queue/SimpleBlockingQueue.java
@@ -9,47 +9,39 @@ import java.util.Queue;
 @ThreadSafe
 public class SimpleBlockingQueue<T> {
 
-    @GuardedBy("monitor")
-    private final Queue<T> queue;
-    private final Object monitor = this;
+    @GuardedBy("this")
+    private final Queue<T> queue = new LinkedList<>();
     private final int capacity;
 
     public SimpleBlockingQueue(int capacity) {
         this.capacity = capacity;
-        queue = new LinkedList<>();
     }
 
-    public void offer(T value) {
-        synchronized (monitor) {
-            while (queue.size() == capacity) {
-                try {
-                    monitor.wait();
-                } catch (InterruptedException e) {
-                    Thread.currentThread().interrupt();
-                }
+    public synchronized void offer(T value) {
+        while (queue.size() == capacity) {
+            try {
+                wait();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
             }
-            queue.offer(value);
-            monitor.notify();
         }
+        queue.offer(value);
+        notifyAll();
     }
 
-    public T poll() {
-        synchronized (monitor) {
-            while (queue.peek() == null) {
-                try {
-                    monitor.wait();
-                } catch (InterruptedException e) {
-                    Thread.currentThread().interrupt();
-                }
+    public synchronized T poll() {
+        while (queue.isEmpty()) {
+            try {
+                wait();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
             }
-            monitor.notify();
-            return queue.poll();
         }
+        notifyAll();
+        return queue.poll();
     }
 
-    public int size() {
-        synchronized (monitor) {
-            return queue.size();
-        }
+    public synchronized int size() {
+        return queue.size();
     }
 }
